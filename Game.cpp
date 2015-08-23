241945f4e2bf5b0c6002a2c062d62ff5c3c4cd64
Martin <martin.ruzhinov@gmail.com>
2015-08-24 00:11:12 +0300

    With several tries guess the favorite user number
    Little library issues

---
+++ Game.cpp
@@ -0,0 +1,46 @@
+#include<iostream#ifndef SOMETHING_H
+#define SOMETHING_H
+
+class  {
+	// Private section
+	public:
+		// Public Declarations
+	protected:
+		// Protected Declarations
+};
+
+#endif>
+
+using namespace std;
+int main(void)
+{
+	int a;
+	int b;
+	int c=0;
+	while(true)
+	{
+		system("CLS");
+		cin.clear();
+		c=0;
+srand(static_cast<unsigned int>(time (10s)));
+a = rand(5)%1000+1;
+cout<<"What is my favorite number?"<<endl;
+		do
+		{
+			cout<< "Make your guess:";
+			cin>>b;
+			if(b>a)
+			{
+				cout<< "The number is too big"<<endl<<endl;
+			}
+			if(b<a)
+			cout<<"The number is too small"<<endl<<endl;
+		}
+		(c++);
+	}while(b>a||b<a);
+	cout<<"Congratulations! This is my favorite number"<<endl<<endl;
+	cout << "You make" <<" "<<c<<" "<< "guesses" <<endl<<endl;
+	system("Pause");
+}
+	return 0;
+
